d47afef84a678673d31d8c5c26808fd07d9bf899
anupam <contact@drafteq.com>
2018-07-19 16:30:53 +0530

    initial commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def _str_(self):
+        return self.question_text
+
+class Choise(models.Model):
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.choise_text
